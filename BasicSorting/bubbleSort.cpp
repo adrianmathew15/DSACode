f97e617b84eb40cf9ca7a03946d7408cf3a27857
Adrian Mathew <adrianaloysius02@gmail.com>
2024-05-29 19:52:01 +0530

    modify

--- BasicSorting/bubbleSort.cpp
+++ BasicSorting/bubbleSort.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 void bubbleSort(int a[], int n) {
+    int count=0;
     for(int times=0; times<n-1; times++) {
         bool flag = false;
         for(int j=0; j<n-times-1; j++) {
             if(a[j] > a[j+1]) {
                 swap(a[j], a[j+1]);
                 flag = true;
+                count++;
             }
         }
 
@@ -16,13 +18,14 @@ void bubbleSort(int a[], int n) {
              break;
          }
     }
+    cout<<"count: "<<count;
 }
 
 int main() {
-    int a[] = {5,4,3,2,1};
-    bubbleSort(a, 5);
+    int a[] = {8, 22, 7, 9, 31, 19, 5, 13};
+    bubbleSort(a, 8);
 
-    for(int x: a) {
-        cout<<x<<" ";
+    for(int i=0; i<8; i++) {
+        cout<<a[i]<<" ";
     }
 }
